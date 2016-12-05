8463e85a53cf595dd62f394a2a0a3aad4eb47dbf
Mateusz Pacholec <mateuszpacholec@gmail.com>
2016-12-05 14:19:39 +0100

    pierwsze repozytorium

---
+++ src/ZadanieLaboratoryjneDotNet/Repository/INewsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZadanieLaboratoryjneDotNet.Model.Models;
+
+namespace ZadanieLaboratoryjneDotNet.Repository
+{
+    public interface INewsRepository
+    {
+        IList<News> GetAllNews();
+    }
+}
---
+++ src/ZadanieLaboratoryjneDotNet/Repository/NewsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZadanieLaboratoryjneDotNet.Model;
+using ZadanieLaboratoryjneDotNet.Model.Models;
+
+namespace ZadanieLaboratoryjneDotNet.Repository
+{
+    public class NewsRepository : INewsRepository
+    {
+        protected DatabaseContext Context { get; set; }
+
+        public NewsRepository(DatabaseContext db)
+        {
+            Context = db;
+        }
+
+        public IList<News> GetAllNews()
+        {
+            return Context.News.ToList();
+        }
+    }
+}
