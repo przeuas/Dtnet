9987d4c278dc6bb1dedd891bd95e769dbfa78b87
Mateusz Pacholec <mateuszpacholec@gmail.com>
2016-12-05 13:20:31 +0100

    skonfigurowanie API, podpięcie swaggera jako dokumentacja i testy do API

---
+++ src/ZadanieLaboratoryjneDotNet/Controllers/api/NewsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZadanieLaboratoryjneDotNet.Controllers.api
+{
+    [Route("api/[controller]")]
+    public class NewsController : Controller
+    {
+        [HttpGet]
+        public ActionResult ApiTest()
+        {
+            return new JsonResult(new
+            {
+                success = true,
+                message = "nasze API działa poprawnie"
+            });
+        }
+
+        [HttpGet("{message}")]
+        public ActionResult ApiTest2(string message)
+        {
+            return new JsonResult(new
+            {
+                success = true,
+                message = message
+            });
+        }
+    }
+}
--- src/ZadanieLaboratoryjneDotNet/Startup.cs
+++ src/ZadanieLaboratoryjneDotNet/Startup.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.Swagger.Model;
 
 namespace ZadanieLaboratoryjneDotNet
 {
@@ -16,6 +20,31 @@ namespace ZadanieLaboratoryjneDotNet
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "WebAPI",
+                    Description = "Proste API do agregacji wiadomości",
+                    TermsOfService = "None"
+                });
+                options.DescribeAllEnumsAsStrings();
+
+                // gdy będziemy używać dokumentacji generowanej z komentarzy to możemy ją zawrzeć w naszym API
+                // options.IncludeXmlComments(Path.ChangeExtension(Assembly.GetEntryAssembly().Location, "xml"));
+            });
+            
+            services.AddMvc()
+                .AddMvcOptions(options =>
+                {
+                    options.CacheProfiles.Add("NoCache", new CacheProfile
+                    {
+                        NoStore = true,
+                        Duration = 0
+                    });
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,9 +57,14 @@ namespace ZadanieLaboratoryjneDotNet
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUi();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("WebAPI - Wytw. oprogram. w środow. NET - Zadanie laboratoryjne");
             });
         }
     }
