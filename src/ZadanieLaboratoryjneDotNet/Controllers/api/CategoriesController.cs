7d379836f14fdb1e3cb64b1f97b9886cd4246cae
Mateusz Pacholec <mateuszpacholec@gmail.com>
2017-01-16 13:57:46 +0100

    Revert "Merge branch 'master' of https://github.com/mati4455/ZadanieDotNet into WykonaneZadanie"
    This reverts commit b374bcc82399e46391a8e7bf1a7700a927248ebb, reversing
    changes made to 7b1b47c01f0340b293aab448696ec5846caecb87.

--- src/ZadanieLaboratoryjneDotNet/Controllers/api/CategoriesController.cs
+++ src/ZadanieLaboratoryjneDotNet/Controllers/api/CategoriesController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ZadanieLaboratoryjneDotNet.Model.Models;
-using ZadanieLaboratoryjneDotNet.Repository;
 
 namespace ZadanieLaboratoryjneDotNet.Controllers.api
 {
@@ -12,34 +10,9 @@ namespace ZadanieLaboratoryjneDotNet.Controllers.api
     public class CategoriesController : Controller
     {
         // deklaracja repozytorium
-        private ICategoriesRepository CategoriesRepository { get; set; }
 
         // konstruktor z wstrzykiwaniem repo
-        public CategoriesController(ICategoriesRepository repo)
-        {
-            CategoriesRepository = repo;
-        }
 
         // metody do obsługi API - GET / POST
-        [HttpGet]
-        public ActionResult GetAll()
-        {
-            return new JsonResult(new
-            {
-                success = true,
-                data = CategoriesRepository.GetAllCategories()
-            });
-        }
-
-        [HttpPost]
-        public ActionResult Post([FromBody] Category kategoria)
-        {
-            kategoria = CategoriesRepository.InsertCategory(kategoria);
-            return new JsonResult(new
-            {
-                success = kategoria.Id > 0, // jeżeli Id > 0 to oznacza, że rekord został poprawnie dodany i baza zwróciła Id
-                data = kategoria
-            });
-        }
     }
 }
--- src/ZadanieLaboratoryjneDotNet/Repository/ICategoriesRepository.cs
+++ src/ZadanieLaboratoryjneDotNet/Repository/ICategoriesRepository.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ZadanieLaboratoryjneDotNet.Model.Models;
 
 namespace ZadanieLaboratoryjneDotNet.Repository
 {
     public interface ICategoriesRepository
     {
-        IList<Category> GetAllCategories();
-        Category InsertCategory(Category kategoria);
     }
 }
